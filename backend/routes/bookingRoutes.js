01786d2213466ce36a073dc2e8c3f5e004d23673
Maruthi Kunarethinam <61498892+KunarathnamMaruthi@users.noreply.github.com>
2026-05-09 12:40:00 +1000

    Added OOP concept project

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -6,40 +6,22 @@ const auth = require("../middleware/auth");
 
 // CREATE
 router.post("/", auth, async (req, res) => {
-  try {
-    const booking = await Booking.create({
-      ...req.body,
-      userId: req.user.id,
-    });
-
-    res.json(booking);
-
-  } catch (err) {
-    res.status(500).json({
-      message: err.message,
-    });
-  }
+  const booking = await Booking.create({
+    ...req.body,
+    userId: req.user.id,
+  });
+  res.json(booking);
 });
 
 // GET
 router.get("/", auth, async (req, res) => {
-  try {
-    if (req.user.isAdmin) {
-      const all = await Booking.find();
-      return res.json(all);
-    }
-
-    const userBookings = await Booking.find({
-      userId: req.user.id,
-    });
-
-    res.json(userBookings);
-
-  } catch (err) {
-    res.status(500).json({
-      message: err.message,
-    });
+  if (req.user.isAdmin) {
+    const all = await Booking.find();
+    return res.json(all);
   }
+
+  const userBookings = await Booking.find({ userId: req.user.id });
+  res.json(userBookings);
 });
 
 module.exports = router;
