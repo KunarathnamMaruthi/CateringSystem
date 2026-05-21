fce9f3c8dd016ab46cfaa053637fa21e09f60208
Maruthi Kunarethinam <61498892+KunarathnamMaruthi@users.noreply.github.com>
2026-05-21 10:37:27 +1000

    Refactor server.js for cleaner code formatting

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,20 @@
-const express =
-  require("express");
+const express = require("express");
 
-const cors =
-  require("cors");
+const cors = require("cors");
 
-const dotenv =
-  require("dotenv");
+const dotenv = require("dotenv");
 
-const connectDB =
-  require("./config/db");
+const connectDB = require("./config/db");
 
-const menuRoutes = 
-require("./routes/menuRoutes");
-
-
-app.use("/api/menu", menuRoutes);
-
-//  CONFIG 
 dotenv.config();
 
-//  DATABASE CONNECTION
+// DATABASE CONNECTION
 connectDB();
 
-// APP 
-const app =
-  express();
+// APP
+const app = express();
 
-//  MIDDLEWARE 
+// MIDDLEWARE
 app.use(cors());
 
 app.use(express.json());
@@ -36,8 +24,7 @@ app.use(
   express.static("uploads")
 );
 
-
-// ROUTES 
+// ROUTES
 app.use(
   "/api/users",
   require("./routes/userRoutes")
@@ -53,25 +40,21 @@ app.use(
   require("./routes/menuRoutes")
 );
 
-//  TEST ROUTE 
+// TEST ROUTE
 app.get("/", (req, res) => {
 
-  res.send(
-    "API running..."
-  );
+  res.send("API running...");
+
 });
 
-//  START SERVER 
+// START SERVER
 const PORT =
-  process.env.PORT ||
-  5000;
+  process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  () => {
+app.listen(PORT, () => {
 
-    console.log(
-      `Server running on port ${PORT}`
-    );
-  }
-);
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
+});
